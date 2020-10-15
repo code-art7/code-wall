f9f3b6a015b06c33a705486e0c118906427d37ef
Gurdayal Singh <guru.gps.20@gmail.com>
2020-10-16 00:07:25 +0530

    searchbar-added

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,16 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("sidenav");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
   }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+}
